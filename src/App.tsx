cf94ae8ef02390de1072b14881ce650d65e52b47
noahvarghese <varghese.noah@gmail.com>
2021-08-04 14:13:24 +0000

    removed the default items, added scss, added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
