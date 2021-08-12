76ffaf9cb934c8d92e169ad3e1fc731d142fd3c8
Noah Varghese <varghese.noah@gmail.com>
2021-08-12 15:34:15 -0400

    got usage of my custom components working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { FileInput, Button } from "@noahvarghese/react-components";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <FileInput name="YOLO" />
+        <Button text="HI" />
       </header>
     </div>
   );
