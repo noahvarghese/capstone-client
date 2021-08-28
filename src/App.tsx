6af0f6c325dc7001a82a58b31bf74155beb2e5c3
noahvarghese <varghese.noah@gmail.com>
2021-08-28 16:14:28 +0000

    got base page loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./App.css";
 import { AppRouter } from "@noahvarghese/react-components";
-import items from "";
 import { connect } from "react-redux";
+import { State } from "./types/state";
 
-const App = () => {
+const App: React.FC = () => {
     return (
         <AppRouter
-            navProps={{ logo: "/logo.png", items, type: "card" }}
+            navProps={{
+                logo: process.env.PUBLIC_URL + "/logo.png",
+                items: [],
+                type: "card",
+            }}
             footerProps={{ copyright: "Noah Varghese 2021" }}
             routes={[]}
         ></AppRouter>
     );
 };
 
-export default connect(({}) => ())(App);
+export default connect(({ auth }: State) => ({
+    auth,
+}))(App);
--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -1,1 +1,8 @@
 import { createReducer } from "../../lib/reduxHelpers";
+import { CustomAction } from "../../types/customAction";
+
+export const setAuth = (_: any, action: CustomAction) => action.payload;
+
+const authReducer = createReducer(false, { SET_AUTH: setAuth });
+
+export default authReducer;
